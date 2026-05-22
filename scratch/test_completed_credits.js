a833e8cf319e6da2d7ca4653c71daf9ea4e3efdf
Ntrungz07 <ntrungz0704@gmail.com>
2026-05-22 12:29:28 +0700

    chore: remove unnecessary scratch files and artifacts

--- scratch/test_completed_credits.js
+++
@@ -1,8 +0,0 @@
-fetch('http://localhost:3000/api/training-info')
-  .then(res => res.json())
-  .then(data => {
-    console.log('--- CURRICULUM ORDER ---');
-    console.log(data.curriculumOrder);
-    console.log('Total subjects in curriculumOrder:', data.curriculumOrder.length);
-  })
-  .catch(console.error);
